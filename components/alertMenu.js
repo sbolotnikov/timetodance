1bf1e52e7cd3e73cb102a4f5d2f28a0cc785ce7c
Sergey Bolotnikov <sbolotnikov@gmail.com>
2023-01-16 13:30:36 -0500

    forgot next build

--- components/alertMenu.js
+++ components/alertMenu.js
@@ -53,12 +53,12 @@ function AllowScroll(){
 }, []);
   return (
 
-    <div className="w-[100vw] h-[100vh] absolute flex justify-center items-center bg-slate-500/70 left-0 z-[1000] backdrop-blur-md" style={{ top: el.scrollTop }} >
+    <div className="w-[100vw] h-[100vh] absolute flex justify-center items-center bg-slate-500/70 left-0 z-[1001] backdrop-blur-md" style={{ top: el.scrollTop }} >
       <div className='m-auto  max-w-[600px] bg-gray-700 border-2 border-solid border-gray-400 rounded-md w-[97%] p-2 flex flex-col content-evenly'>
         <label className='px-1 py-2 border-2 border-solid border-transparent rounded-sm w-full m-1 text-center' style={Object.values(variant)[Object.keys(variant).indexOf(props.styling.variantHead)]}>{props.styling.heading}</label>
         <h5 className="px-1 py-2 border-2 border-solid border-transparent rounded-sm w-full m-1 text-center"  dangerouslySetInnerHTML={{ __html:props.styling.text}}/>
-        {props.styling.inputField && <input id="inputField" className="w-full mb-2 rounded-md text-gray-700" defaultValue={props.inputValue} />}
-        {(props.styling.color1!=="") && <button className='px-1 py-2 border-2 border-solid border-transparent rounded-sm w-full m-1 text-center text-white' style={{backgroundColor:button1Color}} ={e => {AllowScroll(); props.onReturn(props.styling.button1, (props.styling.inputField ? document.querySelector("#inputField").value : null)); }}>
+        {props.styling.inputField && <textarea id="inputField" className="w-full mb-2 rounded-md text-gray-700" defaultValue={props.inputValue} />}
+        {(props.styling.color1!=="") && <button className='px-1 py-2 border-2 border-solid border-transparent rounded-sm w-full m-1 text-center text-white' style={{backgroundColor:button1Color}} onClick={e => {AllowScroll(); props.onReturn(props.styling.button1, (props.styling.inputField ? document.querySelector("#inputField").value : null)); }}>
           {props.styling.button1}
         </button>}
         {(props.styling.color2!=="") &&<button className="px-1 py-2 border-2 border-solid border-transparent rounded-sm w-full m-1 text-center text-white" style={{backgroundColor:button2Color}} onClick={e => {AllowScroll(); props.onReturn(props.styling.button2) }}>
